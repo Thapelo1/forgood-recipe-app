16279722d7f106722f30477716af69ba80818fcc
Thapelo Kekana <thapelo@aglet.co.za>
2018-06-27 10:33:40 +0200

    project upload

---
+++ src/app/db_config/configuration.ts
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: 'AIzaSyADwIWa1HcqDlE4KrUfwQNwK2k0UyTm2iI',
+    authDomain: 'recipe-app-36e02.firebaseapp.com',
+    databaseURL: 'https://recipe-app-36e02.firebaseio.com',
+    projectId: 'recipe-app-36e02',
+    storageBucket: 'recipe-app-36e02.appspot.com',
+    messagingSenderId: '696438048994'
+  };
